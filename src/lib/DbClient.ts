354c81c9b4dfecd00c45a1f8621271b110e59aa6
Lightning K0ala <lightning.k0ala@protonmail.com>
2023-02-05 21:44:08 +0000

    Connection panics when multiple relays

--- src/lib/DbClient.ts
+++ src/lib/DbClient.ts
@@ -17,7 +17,7 @@ export class DbClient {
   constructor({ logger, dbPath }: { logger: Logger; dbPath: string }) {
     this._logger = logger;
     this._db = new PrismaClient({
-      datasources: { db: { url: `file:${dbPath}` } },
+      datasources: { db: { url: `file:${dbPath}?socket_timeout=30&&connection_limit=1` } },
     });
   }
 
